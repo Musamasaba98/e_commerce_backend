075d99ae8b99313a8d3359382fd068edae4b4675
Masaba Musa <masamata98@gmail.com>
2023-08-27 12:23:18 +0300

    Add pagination, filte, sort, limiting fields

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -29,6 +29,16 @@ export const updateProuduct = asyncHandler(async (req, res) => {
         throw new Error(error)
     }
 })
+//delete product
+export const deleteProuduct = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    try {
+        const deleteProduct = await Product.findByIdAndDelete(id)
+        res.json(deleteProduct)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
 //get product
 export const getProduct = asyncHandler(async (req, res) => {
     const { id } = req.params
@@ -42,7 +52,38 @@ export const getProduct = asyncHandler(async (req, res) => {
 //get all products
 export const getAllProducts = asyncHandler(async (req, res) => {
     try {
-        const getAllProducts = await Product.find()
+        //Filtering
+        const queryObj = { ...req.query }
+        const excludeFields = ['page', 'sort', 'limit', 'fields']
+        excludeFields.forEach(el => delete queryObj[el])
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+        let query = Product.find(JSON.parse(queryStr))
+
+        //Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(" ")
+            query = query.sort(sortBy)
+        } else {
+            query = query.sort('-createdAt')
+        }
+        //Limiting Fields  
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(" ")
+            query = query.select(fields)
+        } else {
+            query = query.select('-__v')
+        }
+        //Pagination
+        const page = req.query.page
+        const limit = req.query.limit
+        const skip = (page - 1) * limit
+        query = query.skip(skip).limit(limit)
+        if (req.query.page) {
+            const productCount = await Product.countDocuments()
+            if (skip >= productCount) throw new Error("This page does not exist")
+        }
+        const getAllProducts = await query
         res.json(getAllProducts)
     } catch (error) {
         throw new Error(error)
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { createProuduct, getAllProducts, getProduct, updateProuduct } from '../controller/product.controller.js'
+import { createProuduct, deleteProuduct, getAllProducts, getProduct, updateProuduct } from '../controller/product.controller.js'
+import { authMiddleware, isAdmin } from '../middlewares/authMiddleware.js'
+
 
 const router = express.Router()
 
-router.post('/', createProuduct)
+router.post('/', authMiddleware, isAdmin, createProuduct)
 router.get('/:id', getProduct)
-router.put('/:id', updateProuduct)
+router.put('/:id', authMiddleware, isAdmin, updateProuduct)
+router.delete('/:id', authMiddleware, isAdmin, deleteProuduct)
 router.get('/', getAllProducts)
 
 export default router;
