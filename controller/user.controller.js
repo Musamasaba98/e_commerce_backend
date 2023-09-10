0129d647295a73a95ebbf1077e5f92ac85431a4c
Masaba Musa <masamata98@gmail.com>
2023-09-10 20:20:33 +0300

    Complete order functionality

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,11 +1,16 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
+import Product from "../models/productModel.js";
 import { generateToken } from "../config/jwtToken.js";
 import { validateMongoDbId } from "../utils/validateMongodbId.js";
 import { generateRefreshToken } from "../config/refreshToken.js";
 import jwt from "jsonwebtoken";
 import { sendEmail } from "./email.controller.js";
 import crypto from 'crypto'
+import Cart from "../models/cartModel.js";
+import Order from "../models/orderModel.js";
+import Coupon from "../models/couponModel.js";
+import uniqid from 'uniqid'
 
 //Create a user
 export const createUser = asyncHandler(async (req, res) => {
@@ -267,3 +272,195 @@ export const getUserWishlist = asyncHandler(async (req, res) => {
         throw new Error(error)
     }
 })
+//add to cart
+export const userCart = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    const { cart } = req.body;
+
+    try {
+        // Check inventory availability before adding items to the cart
+        for (let index = 0; index < cart.length; index++) {
+            const product = await Product.findById(cart[index]._id);
+            if (!product || product.quantity < cart[index].count) {
+                return res.status(400).json({ error: 'Insufficient inventory for some items' });
+            }
+        }
+
+        // Calculate the cart total
+        const cartTotal = cart.reduce((total, cartItem) => {
+            const product = cart.find((item) => item._id === cartItem._id);
+            return total + product.price * cartItem.count;
+        }, 0);
+
+        // Check if the user already has a cart
+        const existingCart = await Cart.findOne({ orderby: _id });
+
+        if (existingCart) {
+            // Update the existing cart
+            existingCart.products = cart;
+            existingCart.cartTotal = cartTotal;
+            await existingCart.save();
+        } else {
+            // Create a new cart
+            const newCart = new Cart({
+                products: cart,
+                cartTotal,
+                orderby: _id,
+            });
+            await newCart.save();
+        }
+
+        // Deduct sold quantities from the product inventory
+        for (const cartItem of cart) {
+            const product = await Product.findById(cartItem._id);
+            if (product) {
+                // Deduct sold quantity from available inventory
+                product.quantityAvailable -= cartItem.count;
+                await product.save();
+            }
+        }
+
+        // Return a success response indicating that the order has been placed
+        return res.status(200).json({ message: 'Order placed successfully' });
+    } catch (error) {
+        // Handle errors appropriately, such as sending an error response
+        res.status(500).json({ error: 'Cart update failed' });
+    }
+});
+
+export const getUserCart = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    validateMongoDbId(_id);
+
+    try {
+        const cart = await Cart.findOne({ orderby: _id }).populate('products.product').exec();
+
+        res.json(cart);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+//applying coupon
+export const applyCoupon = asyncHandler(async (req, res) => {
+    const { _id } = req.user;
+    const { orderId } = req.params;
+    const { couponCode } = req.body;
+
+    try {
+        // Validate the user ID and order ID
+        validateMongoDbId(_id);
+        validateMongoDbId(orderId);
+
+        // Find the user's order
+        const userOrder = await Order.findOne({ _id: orderId, orderby: _id });
+
+        if (!userOrder) {
+            return res.status(404).json({ error: 'Order not found' });
+        }
+
+        // Find the coupon by code
+        const coupon = await Coupon.findOne({ name: couponCode });
+
+        if (!coupon) {
+            return res.status(404).json({ error: 'Coupon not found' });
+        }
+
+        // Check if the coupon is valid (e.g., not expired)
+        const currentDate = new Date();
+        if (currentDate > coupon.expiry) {
+            return res.status(400).json({ error: 'Coupon has expired' });
+        }
+
+        // In case I get time I will look into the aspect of coupon limits
+
+        // Apply the coupon to the order (update order total with discount)
+        const discountAmount = (userOrder.cartTotal * coupon.discount) / 100;
+        userOrder.cartTotal -= discountAmount;
+        userOrder.totalAfterDiscount = userOrder.cartTotal;
+
+        // Save the updated order
+        await userOrder.save();
+
+        res.status(200).json({ message: 'Coupon applied successfully' });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Failed to apply coupon' });
+    }
+})
+export const createOrder = asyncHandler(async (req, res) => {
+    const { COD, couponApplied } = req.body
+    const { _id } = req.user
+    try {
+        if (!COD) throw new Error("create cash order failed")
+        const user = await User.findById(_id)
+        const userCart = await Cart.findOne({ orderby: user._id })
+        let finalAmount = 0
+        if (couponApplied && userCart.totalAfterDiscount) {
+            finalAmount = userCart.totalAfterDiscount
+        } else {
+            finalAmount = userCart.cartTotal
+        }
+        let newOrder = await Order({
+            products: userCart.products,
+            paymentIntent: {
+                id: uniqid(),
+                method: "COD",
+                amount: finalAmount,
+                status: "Cash on Delivery",
+                created: Date.now(),
+                currency: "UGX"
+            },
+            orderby: user._id,
+            orderStatus: "Cash on Delivery"
+        }).save()
+        let update = userCart.products.map(item => {
+            return {
+                updateOne: {
+                    filter: { _id },
+                    update: { $inc: { quantity: -item.count, sold: +item.count } }
+                }
+            }
+        })
+        const updated = await Product.bulkWrite(update, {})
+        res.json({ message: "Success" })
+    } catch (error) {
+        throw new Error(error)
+    }
+
+})
+
+export const getOrders = asyncHandler(async (req, res) => {
+    try {
+        const order = await Order.find()
+        res.json(order)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+export const getUserOrders = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    validateMongoDbId(_id)
+    try {
+        const order = await Order.findOne({ orderby: _id }).populate('products.product').exec()
+        res.json(order)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+export const updateOrderStatus = asyncHandler(async (req, res) => {
+    const { status } = req.body
+    const { id } = req.params
+    validateMongoDbId(id)
+    try {
+        const updateOrderStatus = await Order.findByIdAndUpdate(id, {
+            orderStatus: status, paymentIntent: {
+                status: status
+            }
+        }, { new: true })
+        res.json(updateOrderStatus)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createUser, getAllUsers, loginUser, getUser, deleteUser, updateUser, blockUser, unBlockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken, resetPassword, loginAdmin, getUserWishlist, saveAddress } from '../controller/user.controller.js';
+import { createUser, getAllUsers, loginUser, getUser, deleteUser, updateUser, blockUser, unBlockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken, resetPassword, loginAdmin, getUserWishlist, saveAddress, userCart, getUserCart, applyCoupon, createOrder, getOrders, getUserOrders, updateOrderStatus } from '../controller/user.controller.js';
 import { authMiddleware, isAdmin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router()
@@ -8,11 +8,18 @@ router.post("/register", createUser)
 router.post('/forgot-password-token', forgotPasswordToken)
 router.put('/password', authMiddleware, updatePassword)
 router.put('/reset-password/:token', resetPassword)
+router.put('/order/update-order/:id', authMiddleware, isAdmin, updateOrderStatus)
 router.post("/login", loginUser)
-router.post("/admin-login", loginAdmin)
+router.post("/login", loginUser)
+router.post("/add-to-cart", authMiddleware, userCart)
 router.get("/logout", logout)
 router.get('/allUsers', getAllUsers)
 router.get('/wishlist', authMiddleware, getUserWishlist)
+router.get('/cart', authMiddleware, getUserCart)
+router.get('/orders', authMiddleware, isAdmin, getOrders)
+router.get('/user-orders', authMiddleware, isAdmin, getUserOrders)
+router.get('/cart/applycoupon', authMiddleware, applyCoupon)
+router.post('/cart/cash-order', authMiddleware, createOrder)
 router.get('/refresh', handleRefreshToken)
 router.get('/:id', authMiddleware, isAdmin, getUser)
 router.delete('/:id', deleteUser)
