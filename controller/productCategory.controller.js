b0c72f7983607898ca0933f5dc2fe22ef85b7e98
Musamasaba98 <masamata98@gmail.com>
2024-09-14 10:51:05 +0300

    Fix:Remove extra spaces from product category

--- controller/productCategory.controller.js
+++ controller/productCategory.controller.js
@@ -1,52 +1,49 @@
 import PCategory from "../models/productCategoryModel.js";
 import asyncHandler from "express-async-handler";
 
-
-
-
 export const createCategory = asyncHandler(async (req, res) => {
-    try {
-        const newCategory = await PCategory.create(req.body)
-        res.json(newCategory)
-    } catch (error) {
-        throw new Error(error)
-    }
-})
+  try {
+    const newCategory = await PCategory.create(req.body);
+    res.json(newCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 export const updateCategory = asyncHandler(async (req, res) => {
-    try {
-        const { id } = req.params
-        const updatedCategory = await PCategory.findByIdAndUpdate(id, req.body, {
-            new: true
-        })
-        res.json(updatedCategory)
-    } catch (error) {
-        throw new Error(error)
-    }
-})
+  try {
+    const { id } = req.params;
+    const updatedCategory = await PCategory.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 export const deleteCategory = asyncHandler(async (req, res) => {
-    try {
-        const { id } = req.params
-        const deletedCategory = await PCategory.findByIdAndDelete(id)
-        res.json(deletedCategory)
-    } catch (error) {
-        throw new Error(error)
-    }
-})
+  try {
+    const { id } = req.params;
+    const deletedCategory = await PCategory.findByIdAndDelete(id);
+    res.json(deletedCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 export const getCategory = asyncHandler(async (req, res) => {
-    try {
-        const { id } = req.params
-        const getaCategory = await PCategory.findById(id)
-        res.json(getaCategory)
-    } catch (error) {
-        throw new Error(error)
-    }
-})
+  try {
+    const { id } = req.params;
+    const getaCategory = await PCategory.findById(id);
+    res.json(getaCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 export const getAllCategory = asyncHandler(async (req, res) => {
-    try {
-        const allCategory = await PCategory.find()
-        res.json(allCategory)
-    } catch (error) {
-        throw new Error(error)
-    }
-})
+  try {
+    const allCategory = await PCategory.find();
+    res.json(allCategory);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
